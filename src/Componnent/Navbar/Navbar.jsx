58a67f9739e059100ac82a1eab84598766e6c17e
yomna062 <mohamedyomna212@gmail.com>
2025-11-15 17:00:53 +0200

    solve imgs error

--- src/Componnent/Navbar/Navbar.jsx
+++ src/Componnent/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../Assets/logo.png';
+import logo from '../../assets/logo.png';
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
